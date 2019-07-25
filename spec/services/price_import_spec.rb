527e5121adbc0847fa9bfb81cf44a57f2a8ef8d0
Ruslan Valeev <ruvaleev@gmail.com>
2019-07-26 00:29:09 +0700

    price import spec fixed

--- spec/services/price_import_spec.rb
+++ spec/services/price_import_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PriceImport do
-  let!(:price_csv_file) { File.read(File.join Rails.root, 'spec', 'fixtures', 'files', 'price.csv') }
+  let!(:price_csv_file) { File.read(Rails.root.join('spec', 'fixtures', 'files', 'price.csv')) }
   let!(:processor) { described_class.new(price_csv_file) }
   let(:new_row) do
     "\r\nHP NewSeries NewPrinterModel_1 / NewPrinterModel_2 / NewPrinterModel_3,Cartridge Model_1,30000,0,0,0,1000,0"
@@ -21,7 +21,7 @@ RSpec.describe PriceImport do
     it 'creates printer service guides' do
       expect { processor.run }.to_not change(PrinterServiceGuide, :count)
     end
-    
+
     it 'creates cartridge service guides' do
       expect { processor.run }.to_not change(CartridgeServiceGuide, :count)
     end
@@ -31,8 +31,8 @@ RSpec.describe PriceImport do
     before do
       price_csv_file.concat(new_row)
       described_class.new(price_csv_file).run
-      @printer = PrinterServiceGuide.find_by_model('NewPrinterModel_1')
-      @cartridge = CartridgeServiceGuide.find_by_model('Cartridge Model_1')
+      @printer = PrinterServiceGuide.find_by(model: 'NewPrinterModel_1')
+      @cartridge = CartridgeServiceGuide.where(model: 'Cartridge Model_1').first
     end
 
     it 'creates correct associations' do
