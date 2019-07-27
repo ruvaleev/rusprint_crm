1f9b31f941e4a9fe8b2df9f0c3c73ffda7251bc2
Ruslan Valeev <ruvaleev@gmail.com>
2019-07-27 23:52:56 +0700

    csg controller spec fixed

--- app/controllers/cartridge_service_guides_controller.rb
+++ app/controllers/cartridge_service_guides_controller.rb
@@ -1,16 +1,30 @@
 class CartridgeServiceGuidesController < ApplicationController
+  before_action :find_printer
+  before_action :find_printer_service_guide
+  after_action :transfer_variables
   load_and_authorize_resource
 
   def create
     @cartridge_service_guide = CartridgeServiceGuide.create(create_params)
-    @shopping_cart_id = params[:shopping_cart_id]
-    @printer = Printer.find_by(id: params[:printer_id])
-    @order_id = params[:order_id]
+    @printer_service_guide.cartridges.push(@cartridge_service_guide) if @cartridge_service_guide.persisted?
   end
 
   private
 
+  def find_printer
+    @printer = Printer.find_by(id: params[:printer_id])
+  end
+
+  def find_printer_service_guide
+    @printer_service_guide = PrinterServiceGuide.find(params[:printer_service_guide_id])
+  end
+
   def create_params
     params.require(:cartridge_service_guide).permit!
+  end
+
+  def transfer_variables
+    @shopping_cart_id = params[:shopping_cart_id]
+    @order_id = params[:order_id]
   end
 end
--- spec/controllers/cartridge_service_guides_controller_spec.rb
+++ spec/controllers/cartridge_service_guides_controller_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe CartridgeServiceGuidesController, type: :controller do
   describe 'POST #create' do
+    let!(:printer_service_guide) { create(:printer_service_guide) }
     context 'Authorized user' do
       sign_in_user
 
       it 'creates with valid attributes' do
         expect do
-          post :create, params: { cartridge_service_guide: build(:cartridge_service_guide).attributes, format: :js }
+          post :create, params: { cartridge_service_guide: build(:cartridge_service_guide).attributes,
+                                  printer_service_guide_id: printer_service_guide.id, format: :js }
         end.to change(CartridgeServiceGuide, :count).by(1)
       end
 
       it "can't create with invalid attributes (without cartridge_service_guide_id)" do
         expect do
-          post :create, params: {
-            cartridge_service_guide: build(:cartridge_service_guide).attributes.except('model'), format: :js
-          }
+          post :create, params: { cartridge_service_guide: build(:cartridge_service_guide).attributes.except('model'),
+                                  printer_service_guide_id: printer_service_guide.id, format: :js }
         end.to_not change(CartridgeServiceGuide, :count)
       end
     end
@@ -23,7 +24,8 @@ RSpec.describe CartridgeServiceGuidesController, type: :controller do
     context 'Unauthorized user' do
       it "can't create Cartridge Service Guide" do
         expect do
-          post :create, params: { cartridge_service_guide: build(:cartridge_service_guide).attributes, format: :js }
+          post :create, params: { cartridge_service_guide: build(:cartridge_service_guide).attributes,
+                                  printer_service_guide_id: printer_service_guide.id, format: :js }
         end.to_not change(CartridgeServiceGuide, :count)
       end
     end
