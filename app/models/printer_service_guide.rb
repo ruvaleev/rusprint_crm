86827ea11e7b285e49c7e917efe89f8d417e8926
Ruslan Valeev <ruvaleev@gmail.com>
2019-07-28 18:30:50 +0700

    validation of uniqueness added to printer service guide model

--- app/models/printer_service_guide.rb
+++ app/models/printer_service_guide.rb
@@ -2,4 +2,5 @@ class PrinterServiceGuide < ApplicationRecord
   has_many :printers_compatibilities, dependent: :destroy
   has_many :cartridges, through: :printers_compatibilities, source: :compatible, source_type: 'CartridgeServiceGuide'
   validates :model, :type_of_system, presence: true
+  validates :model, uniqueness: { scope: :vendor }
 end
